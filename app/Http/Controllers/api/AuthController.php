3c5c3fe527bc587ce70041c171f365269cf9914b
Mohammed Salah Zourob <mohammed.dev.2017@gmail.com>
2024-10-25 13:47:10 +0300

    add register function

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -4,8 +4,35 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+
+        $rules=[
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email',
+            'password'=>['required','string','min:8','confirmed']
+        ];
+
+        $validation=Validator::make($request->all(),$rules);
+
+        if($validation->fails())
+        {
+            return parent::error($validation->errors());
+        }
+        $request['password']=Hash::make($request->input('password'));
+        $user = User::create($request->all());
+
+        if ($request->has('fcm_token')) {
+            Token_firebase::create(['fcm_token' => $request->get('fcm_token'), 'user_id' => $user->id]);
+        }
+        $success['token'] =  $user->createToken($user->name)->plainTextToken;
+        return parent::success($success);
+
+    }
 }
