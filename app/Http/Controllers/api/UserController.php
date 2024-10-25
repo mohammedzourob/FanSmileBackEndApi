6a529ea103d9af8084290b4d8a8768b324445d3d
Mohammed Salah Zourob <mohammed.dev.2017@gmail.com>
2024-10-25 16:58:36 +0300

    auth check user login

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use  App\Models\User;
+use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
 
 
-    public function users() {
+    public function user() {
 
-        $user=User::All();
-
-        return parent::success($user);
-
+        if(Auth::check())
+        {
+            $user = Auth::user();
+            return parent::success($user);
+        }
     }
+
+    
 }
