7b9b48cb67ec7e70c67e69e0c0e4cfcbfafe41e7
Mohammed Salah Zourob <mohammed.dev.2017@gmail.com>
2024-10-25 11:21:00 +0300

    add success and error methods to use in api controllsers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,22 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public static function success($message,$status = 200)
+    {
+        return response()->json(['status' => 'success','errors'=> 0 ,'code' => $status, 'data' => $message],$status)->header('Content-type','application/json');
+    }
+
+    public static function error($message , $status = 400)
+    {
+        $messageCount = 1;
+        if (is_array($message))
+            $messageCount = sizeof($messageCount);
+        elseif ($message instanceof Collection){
+            $messageCount =$message->count();
+        }
+        if ($message instanceof MessageBag)
+            $message= $message->first();
+        return response()->json(['status' => 'error','errors'=> $messageCount , 'data' => $message],$status)->header('Content-type','application/json');
+
+    }
 }
