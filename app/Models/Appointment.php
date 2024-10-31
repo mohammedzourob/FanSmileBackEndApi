f4d69ef6f2a534763afdfbd027621ac89caf49e5
Mohammed Salah Zourob <mohammed.dev.2017@gmail.com>
2024-10-31 14:01:32 +0200

    create appointment model and add data

---
+++ app/Models/Appointment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    protected $fillable=[
+    'userId',
+    'patientId',
+    'startDate',
+    'endDate',
+    'details'
+    ];
+
+
+
+    public function users()
+    {
+        return $this->belongsTo(User::class,'userId','id');
+    }
+
+    public function patients()
+    {
+        return $this->belongsTo(Patient::class,'patientId','id');
+    }
+
+}
