38b4f5d20abbbd208ba6c60cc607986da1af8a7f
Mohammed Salah Zourob <mohammed.dev.2017@gmail.com>
2024-10-31 09:59:16 +0200

    add auth routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// Route::middleware(['isAdmin'])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return 'Dashboard';
+//     });
+
+// });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
