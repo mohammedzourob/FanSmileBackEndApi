638d920fe83d64400b1a4e8d0e973fe1b60b25d4
Mohammed Salah Zourob <mohammed.dev.2017@gmail.com>
2024-11-03 10:23:55 +0200

    create and add roules UpdateAppointmentRequest

---
+++ app/Http/Requests/appointment/UpdateAppointmentRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\appointment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAppointmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'userId'=>'nullable',
+            'patientId'=>'nullable',
+            'startDate'=>['nullable', 'date', 'after_or_equal:today'],
+            'endDate' => ['nullable', 'date', 'after_or_equal:today'],
+            'details'=>'nullable'
+        ];
+    }
+}
