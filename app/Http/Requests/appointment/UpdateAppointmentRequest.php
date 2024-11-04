98b09f97103f55028c24536472874bd43fd8047a
Mohammed Salah Zourob <mohammed.dev.2017@gmail.com>
2024-11-04 07:40:00 +0200

    add messages rules appointment

--- app/Http/Requests/appointment/UpdateAppointmentRequest.php
+++ app/Http/Requests/appointment/UpdateAppointmentRequest.php
@@ -22,11 +22,26 @@ class UpdateAppointmentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'userId'=>'nullable',
-            'patientId'=>'nullable',
+            'userId'=>'required|integer|min:1|max:9223372036854775807',
+            'patientId'=>'required|integer|min:1|max:9223372036854775807',
             'startDate'=>['nullable', 'date', 'after_or_equal:today'],
             'endDate' => ['nullable', 'date', 'after_or_equal:today'],
             'details'=>'nullable'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return[
+            'userId.required' => 'The user ID is required.',
+            'userId.integer' => 'The user ID must be a valid integer.',
+            'userId.min' => 'The user ID cannot be less than 1',
+            'userId.max' => 'The user ID cannot be greater than 9,223,372,036,854,775,807.',
+
+            'patientId.required' => 'The patient ID is required.',
+            'patientId.integer' => 'The patient ID must be a valid integer.',
+            'patientId.min' => 'The patient ID cannot be less than 1.',
+            'patientId.max' => 'The patient ID cannot be greater than 9,223,372,036,854,775,807.',
         ];
     }
 }
