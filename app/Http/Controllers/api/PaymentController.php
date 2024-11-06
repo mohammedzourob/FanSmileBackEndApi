43e0639de5e32f2f1ce46ddb675a0adc1493e5d3
Mohammed Salah Zourob <mohammed.dev.2017@gmail.com>
2024-11-06 12:37:18 +0200

    create payment controller and add store and update functions

---
+++ app/Http/Controllers/api/PaymentController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Payment\StorePaymentRequest;
+use App\Http\Requests\Payment\UpdatePaymentRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\models\Payment;
+
+class PaymentController extends Controller
+{
+
+
+    public function store(StorePaymentRequest $request):JsonResponse
+    {
+
+        $validation=$request->validated();
+        $total=$request['totalAmount'];
+        $first=$request['firstAmount'];
+
+        $request['remainingAmount']=$total-$first;
+        $payment=Payment::create($request->all());
+
+        return parent::success('payment has added successfully');
+    }
+
+    public function update(UpdatePaymentRequest $request,$id)
+    {
+        $validation=$request->validated();
+        $payment=Payment::findOrFail($id);
+        $total=$request['totalAmount'];
+        $first=$request['firstAmount'];
+
+        $payment['remainingAmount']=$total-$first;
+        $payment->update($validation);
+
+        return parent::success('payment has added successfully');
+    }
+}
